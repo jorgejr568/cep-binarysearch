3c38fdc7b1ab0d2a82126db9bb8414ca837d5b5e
Jorge Junior <jorge.junior.568@live.com>
2018-03-24 17:24:15 -0300

    Invalid line exclamation

--- app/exception/InvalidLineAddress.php
+++ app/exception/InvalidLineAddress.php
@@ -13,7 +13,7 @@ use Throwable;
 
 class InvalidLineAddress extends \Exception
 {
-    public function __construct($message = "INVALID ADDRESS LINE", $code = 500, Throwable $previous = null)
+    public function __construct($message = "INVALID ADDRESS LINE!", $code = 500, Throwable $previous = null)
     {
         parent::__construct($message, $code, $previous);
     }

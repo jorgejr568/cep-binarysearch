6a7cdb1211a546f809b1b26a27f4c0ab5863e2bb
Jorge Junior <jorge.junior.568@live.com>
2018-03-24 14:13:54 -0300

    InvalidAddressLine exception created

---
+++ app/exception/InvalidLineAddress.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gpca
+ * Date: 22/03/18
+ * Time: 17:53
+ */
+
+namespace CEPSearcher\Exception;
+
+
+use Throwable;
+
+class InvalidLineAddress extends \Exception
+{
+    public function __construct($message = "INVALID ADDRESS LINE", $code = 500, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}

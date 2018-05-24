d6d7f4b88ba8c4c06052ee16cbbed632fde7df48
caiopotter <limapotter@gmail.com>
2018-05-24 19:05:36 -0300

    Prova refractor controller created with exercises

---
+++ app/controller/ProvaRefractorController.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aluno
+ * Date: 24/05/18
+ * Time: 19:04
+ */
+
+namespace CEPSearcher\Controller;
+
+
+class ProvaRefractorController
+{
+    public function exercise1(){
+
+    }
+
+    public function exercise2(){
+
+    }
+
+    public function exercise3(){
+
+    }
+}

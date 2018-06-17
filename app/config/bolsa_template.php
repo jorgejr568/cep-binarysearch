bb13c6de9007b40e40daaf31416605fcacc6c368
Jorge Junior <jorge.junior.568@live.com>
2018-06-16 21:09:51 -0300

    Adding bolsa template config

---
+++ app/config/bolsa_template.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'UF',
+	'CodigoSIAFIMunicipio',
+	'NomeMunicipio',
+	'CodigoFuncao',
+    'CodigoSubfuncao',
+	'CodigoPrograma',
+	'CodigoAcao',
+	'NISFavorecido',
+    'NomeFavorecido',
+	'FonteFinalidade',
+	'ValorParcela',
+	'MesCompetencia'
+];

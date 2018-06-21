3610a45e392dbc7e594ff1db1630732792ba9c54
Jorge Junior <jorge.junior.568@live.com>
2018-06-21 00:04:57 -0300

    Updating docs

--- install.sh
+++ install.sh
@@ -2,8 +2,14 @@ echo "-> CLONING REPOSITORY..." &&
 git clone https://github.com/jorgejr568/file-structure-organization-algorithms.git &&
 echo "-> ENTERING ON PROJECT DIRECTORY" &&
 cd file-structure-organization-algorithms/ &&
+echo "-> DOWNLOADING BOLSA ZIP FILE" &&
+wget https://jorgejuniorx.com.br/projects/cefet-rj/file-structure-organization-algorithms/app/data/bolsa.zip -O ./app/data/bolsa.zip
+echo "-> UNZIPING BOLSA DAT FILE" &&
+unzip -d ./app/data ./app/data/bolsa.zip &&
 echo "-> UNZIPING CEP DAT FILE" &&
 unzip -d ./app/data ./app/data/cep.zip &&
+echo "-> MAPPING BOLSA PAK FILES" &&
+php ./bolsa-hash-generator.php &&
 echo "-> REMOVING INSTALL SCRIPT" &&
 rm ../install.sh &&
 echo "-> YOU'RE ALL SET UP!"
